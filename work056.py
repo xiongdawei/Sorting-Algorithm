95a92f85ca27acea38e869407bed153df564fd3c
Paddingtondx <42790040+xiongdawei@users.noreply.github.com>
2018-11-05 10:24:33 +0800

    Update work056.py

--- work056.py
+++ work056.py
@@ -38,7 +38,67 @@ def quicksorthelper(alist,first,last):
     quicksorthelper(alist,first,splitpoint-1)
     quicksorthelper(alist,splitpoint+1,last)
 
+def selectionsort(x):
+    a = []
+    while len(x)!=0:
+        minn = x[0]
+        for i in range(len(x)):
+            if x[i]<minn:
+                minn = x[i]
+        a.append(minn)
+        x.remove(minn)
+    return a
+  
+  
+def merge(left,right):
+    result = []
+    i,j=0,0
+    while i<len(left) and j<len(right):
+        if left[i]<right[j]:
+            result.append(left[i])
+            i+=1
+        else:
+            result.append(right[j])
+            j+=1
+    while i<len(left):
+        result.append(left[i])
+        i+=1
+    while j<len(right):
+        result.append(right[j])
+        j+=1
+    return result
 
+def merge_sort(L):
+    if len(L)<2:
+        return L[:]
+    else:
+        mid = int(len(L)/2)
+        left = merge_sort(L[:mid])
+        right = merge_sort(L[mid:])
+        return merge(left,right)
+      
+def insertionsort(A):
+    for j in range(1,len(A)):
+        key = A[j]
+        i = j-1
+        while i>=0 and A[i] > key:
+            A[i+1] = A[i]
+            
+            i-=1
+        A[i+1] = key
+    return A
+  
+  
+def bubblesort(a):
+    length = len(a)
+    while length>0:
+        for i in range(len(a)-1):
+            if a[i]>a[i+1]:
+                a[i],a[i+1]=a[i+1],a[i]
+                
+        length-=1
+    return a
+ 
 
   
 a = [109,34,22,202,5,1,6,56,23,12,67,40,9,23,54]
